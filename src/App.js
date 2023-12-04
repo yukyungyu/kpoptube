6c20e876cd1a5b46c71aaf50eea09d20cc646b73
yukyungyu <kyk7497@naver.com>
2023-12-04 21:47:37 +0900

    페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Today from "./pages/Today";
+import Artist from "./pages/Artist";
+import Youtube from "./pages/Youtube";
+import Channel from "./pages/Channel";
+import Video from "./pages/Video";
+import Search from "./pages/Search";
+import Not from "./pages/Not";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/today" element={<Today />} />
+        <Route path="/artist" element={<Artist />} />
+        <Route path="/youtube" element={<Youtube />} />
+        <Route path="/channel" element={<Channel />} />
+        <Route path="/video" element={<Video />} />
+        <Route path="/search/:searchId" element={<Search />} />
+        <Route path="*" element={<Not />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
