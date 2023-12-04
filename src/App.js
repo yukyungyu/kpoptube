d35926f1d7e3f2b4356886d46ae6805fe8347c60
yukyungyu <kyk7497@naver.com>
2023-12-04 22:03:20 +0900

    섹션 컴퍼넌트 구조화

--- src/App.js
+++ src/App.js
@@ -9,19 +9,27 @@ import Video from "./pages/Video";
 import Search from "./pages/Search";
 import Not from "./pages/Not";
 
+import Header from "./components/section/Header";
+import Main from "./components/section/Main";
+import Footer from "./components/section/Footer";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/today" element={<Today />} />
-        <Route path="/artist" element={<Artist />} />
-        <Route path="/youtube" element={<Youtube />} />
-        <Route path="/channel" element={<Channel />} />
-        <Route path="/video" element={<Video />} />
-        <Route path="/search/:searchId" element={<Search />} />
-        <Route path="*" element={<Not />} />
-      </Routes>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/artist" element={<Artist />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/channel" element={<Channel />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/search/:searchId" element={<Search />} />
+          <Route path="*" element={<Not />} />
+        </Routes>
+      </Main>
+      <Footer />
     </BrowserRouter>
   );
 }
