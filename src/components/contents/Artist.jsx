e3c1dfee10ec0ebf51c0dc0a2d133865b6c39678
yukyungyu <kyk7497@naver.com>
2023-12-09 21:11:33 +0900

    추천 아티스트 작업

--- src/components/contents/Artist.jsx
+++ src/components/contents/Artist.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+
+import { artistText } from "../../data/artist";
 
 const Artist = () => {
-  return <div>Artist</div>;
+  return (
+    <section id="artist">
+      <h2>추천 아티스트</h2>
+      <div className="artist__inner">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={15}
+          navigation={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 5,
+              spaceBetween: 15,
+            },
+            768: {
+              slidesPerView: 6,
+              spaceBetween: 15,
+            },
+            1024: {
+              slidesPerView: 7,
+              spaceBetween: 15,
+            },
+            1240: {
+              slidesPerView: 8,
+              spaceBetween: 15,
+            },
+            1640: {
+              slidesPerView: 9,
+              spaceBetween: 15,
+            },
+            2000: {
+              slidesPerView: 10,
+              spaceBetween: 20,
+            },
+          }}
+          modules={[Autoplay, Navigation]}
+          className="mySwiper"
+        >
+          {artistText.map((artist, key) => (
+            <SwiperSlide key={key}>
+              <div className="artist">
+                <div className="artist__img play__icon">
+                  <Link to={`/channel/${artist.channelId}`}>
+                    <img src={artist.img} alt={artist.name} />
+                  </Link>
+                </div>
+                <div className="artist__info">
+                  <Link to={`/channel/${artist.channelId}`}>{artist.name}</Link>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 };
 
 export default Artist;
--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Main from "../components/section/Main";
 
+import { Link } from "react-router-dom";
+import { artistText } from "../data/artist";
+
 const Artist = () => {
   return (
     <Main title="케이팝 아티스트" description="케이팝 아티스트 페이지입니다.">
-      Artist
+      <section id="artistPage">
+        <h2>추천 아티스트</h2>
+        <div className="artist__inner">
+          {artistText.map((artist, key) => (
+            <div className="artist" key={key}>
+              <div className="artist__img play__icon">
+                <Link to={`/channel/${artist.channelId}`}>
+                  <img src={artist.img} alt={artist.name} />
+                </Link>
+              </div>
+              <div className="artist__info">
+                <Link to={`/channel/${artist.channelId}`}>{artist.name}</Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </Main>
   );
 };
